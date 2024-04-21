711f8c66c4ebc16ef6b5131d06b8d3b1f01cac7d
valliyappan-18 <valliyappan02@gmail.com>
2024-04-22 04:50:10 +0530

    project updated

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,3 +6,16 @@ logoToRedirectHome.addEventListener('click',function(){
     window.location.href="index.html";
     console.log('Logo button clicked');
 })
+
+// menu icon click event
+
+let menuBtn = document.querySelector('.menu-icon');
+
+let navbar = document.querySelector('.container1-contents');
+
+let sectionContents = document.querySelector('.Home-Contents');
+
+menuBtn.addEventListener('click',function(){
+    navbar.classList.toggle('active');
+    sectionContents.classList.toggle('blur');
+})
