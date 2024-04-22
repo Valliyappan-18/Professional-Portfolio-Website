b26caeaabe444a21fa0eb699dfeaf11416813e09
valliyappan-18 <valliyappan02@gmail.com>
2024-04-22 07:22:28 +0530

    index update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,7 +3,7 @@
 var logoToRedirectHome = document.querySelector('.logo');
 
 logoToRedirectHome.addEventListener('click',function(){
-    window.location.href="index.html";
+    window.location.href="/index.html";
     console.log('Logo button clicked');
 })
 
